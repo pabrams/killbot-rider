75260c8a7a9c5c968fe9792ea65cf1947541ac49
Paul Abrams <john.paul.abrams@gmail.com>
2022-02-20 01:34:05 -0500

    doc: combatAction

--- src/CombatAction.ts
+++ src/CombatAction.ts
@@ -1,37 +1,41 @@
 
 /*
 
-ACTIVE
+Rough notes
+
+CALC per turn
+    fatigue added: fatigueRate
+    Action points (AP): piloting, tactics, -fatigue
+    Initiative (turn sequence): reaction -fatigueRate/2
+    Ally bonus to all stats: leadership
+    Chance to eject: damage, heat, -bravery, fatigue
+
+Player Actions
 
 Fire a laser at a target
-    chance to hit: aim -fatigueRate/2
+    chance to hit: aim -fatigue/2
     damage: weapon power
-    chance to crit: tactics -fatigueRate/2
-    speed (AP cost): aim, reaction, -fatigueRate/2
+    chance to crit: tactics -fatigue/2
+    speed (AP cost): aim, reaction, -fatigue/2
 
 Fire ballistics at a target
-    chance to hit: aim, tactics -fatigueRate
+    chance to hit: aim, tactics -fatigue
     damage: weapon power
 
 Move to a location
-    speed (AP cost): piloting -fatigueRate/2
-    chance to topple: piloting, tactics -fatigueRate/2
+    speed (AP cost): piloting -fatigue/2
+    chance to topple: piloting, tactics -fatigue/2
 
 Prepare reaction fire
-    chance to hit: aim -fatigueRate/2
-    chance to fire: reaction - fatigueRate/2
 
 Repair killbot
     repair amount: repair
     AP cost: repair
 
-PASSIVE
-
-Action points (AP): piloting, tactics, -fatigueRate
-Initiative: reaction -fatigueRate/2
-Ally bonus to all stats: leadership
-Chance to miss a turn: -fatigueRate -bravery
-Evasion: piloting, tactics
-
+Enemy actions
+    Evasion: piloting, tactics, reaction, -fatigue/2
+    reaction fire chance to fire: reaction - fatigue/2
+    reaction fire chance to hit: aim -fatigue/2
+    
 
 */
