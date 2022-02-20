2bf2fd7fa43c1654aeabf95d9f6bdca4b3680a5a
Paul Abrams <john.paul.abrams@gmail.com>
2022-02-20 01:33:35 -0500

    docs: rough notes on action and bot

---
+++ src/Killbot.ts
@@ -0,0 +1,20 @@
+/*
+
+ATTRIBUTES
+weapon mount points
+engine
+structure: speed
+structure: stability
+chassis size
+armor
+legs
+wheels 
+track
+
+STATS
+weight: mount points weight + engine weight
+speed: - weight + engine
+stability
+energy
+]
+*/
